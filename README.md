1d52134e8402ec31feaead1224a88b099d83d897
biancahc <biancacarolinecs@gmail.com>
2024-01-30 10:48:24 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+<h1>Decodificador de Texto - Alura Challenges Oracle ONE</h1>
+<h3>Desafio Challenge ONE inciante em progrmação alura</h3>
+
+<h2>🔖 Sobre</h2>
+
+<h5>As "chaves" de criptografia que utilizaremos são:</h5>
+<p>A letra "e" é convertida para "enter"</p>
+<p>A letra "i" é convertida para "imes"</p>
+<p>A letra "a" é convertida para "ai"</p>
+<p>A letra "o" é convertida para "ober"</p>
+<p>A letra "u" é convertida para "ufat"</p>
+
+<h3>Requisitos:</h3>
+<p>- Deve funcionar apenas com letras minúsculas</p>
+<p>- Não devem ser utilizados letras com acentos nem caracteres especiais</p>
+<p>
+  - Deve ser possível converter uma palavra para a versão criptografada e também
+  retornar uma palavra criptografada para a versão original.
+</p>
+
+<h3>Por exemplo:</h3>
+<p>"gato" => "gaitober"</p>
+<p>gaitober" => "gato"</p>
+
+<h3>Disafio extra:</h3>
+<p>
+  - Um botão que copie o texto criptografado/descriptografado para a área de
+  transferência - ou seja, que tenha a mesma funcionalidade do ctrl+C ou da
+  opção "copiar" do menu dos aplicativos.
+</p>
+
+<h3>🚀 Tecnologias:</h3>
+
+<div>
+  <img
+    src="https://img.shields.io/badge/HTML-239120?style=for-the-badge&logo=html5&logoColor=white"
+  />
+  <img
+    src="https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white"
+  />
+  <img
+    src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black"
+  />
+</div>
+
+<h2>
+  <a href="https://github.com/biancahc">Bianca Caroline (Github) - 2024</a>
+</h2>
---
+++ app.js
@@ -0,0 +1,64 @@
+function codificaTexto() {
+  let textoCodificado = '';
+  let textoCodificar = document.getElementById('texto-codificar').value;
+
+  if (/[A-Z\u00C0-\u00FF ]/.test(textoCodificar)) {
+    alert('Apenas letras minúsculas e sem acento!');
+    return;
+  }
+
+  for (let i = 0; i < textoCodificar.length; i++) {
+    let letraAtual = textoCodificar[i];
+    if (letraAtual == 'e') {
+      textoCodificado += 'enter';
+    }
+    else if (letraAtual == 'i') {
+      textoCodificado += 'imes';
+    }
+    else if (letraAtual == 'a') {
+      textoCodificado += 'ai';
+    }
+    else if (letraAtual == 'o') {
+      textoCodificado += 'ober';
+    }
+    else if (letraAtual == 'u') {
+      textoCodificado += 'ufat';
+    } else {
+      textoCodificado += letraAtual;
+    }
+  }
+  document.getElementById('texto-codificado').value = textoCodificado;
+
+  if (textoCodificado.length > 0) {
+    document.getElementById('texto-preenchido').style.display = 'flex';
+    document.getElementById('texto-vazio').style.display = 'none';
+  } else {
+    document.getElementById('texto-preenchido').style.display = 'none';
+    document.getElementById('texto-vazio').style.display = 'flex';
+  }
+}
+
+function decodificaTexto() {
+  let textoDecodificado = document.getElementById('texto-codificar').value;
+  textoDecodificado = textoDecodificado.replace(/enter/g, 'e');
+  textoDecodificado = textoDecodificado.replace(/imes/g, 'i');
+  textoDecodificado = textoDecodificado.replace(/ai/g, 'a');
+  textoDecodificado = textoDecodificado.replace(/ober/g, 'o');
+  textoDecodificado = textoDecodificado.replace(/ufat/g, 'u');
+
+  document.getElementById('texto-codificado').value = textoDecodificado;
+
+  if (textoDecodificado.length > 0) {
+    document.getElementById('texto-preenchido').style.display = 'flex';
+    document.getElementById('texto-vazio').style.display = 'none';
+  } else {
+    document.getElementById('texto-preenchido').style.display = 'none';
+    document.getElementById('texto-vazio').style.display = 'flex';
+  }
+}
+
+function copiarTexto() {
+  let textoCopiado = document.getElementById('texto-codificado').value;
+  navigator.clipboard.writeText(textoCopiado);
+}
+
